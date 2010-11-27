86605a5353ded4b76dc5c641096a91fe81113805
Josh Joyce <jnjoyce@speakeasy.net>
2010-11-26 22:27:41 -0600

    added proguard

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -1,7 +1,22 @@
 import sbt._
 
-class Project(info: ProjectInfo) extends DefaultProject(info) {
-  val httpClient = "commons-httpclient" % "commons-httpclient" % "3.1"
+class Project(info: ProjectInfo) extends DefaultProject(info) with ProguardProject {
   val commonsCodec = "commons-codec" % "commons-codec" % "1.4"
   val commonsConfig = "commons-configuration" % "commons-configuration" % "1.6"
+  
+  override val artifactID = "gasbag"
+  
+  override def mainClass: Option[String] = None
+  
+  override def proguardOptions = List(
+    "-dontoptimize",
+    "-dontobfuscate",
+    proguardKeepLimitedSerializability,
+    proguardKeepAllScala,
+    "-keep interface scala.ScalaObject",
+    "-keep class org.joshjoyce.gasbag.Gasbag",
+    "-dontshrink"
+  )
+    
+  override def proguardInJars = Path.fromFile(scalaLibraryJar) +++ super.proguardInJars
 }
